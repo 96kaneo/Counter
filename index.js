dbda7b096daba829a70261deea0a5d7966ff1f11
96kaneo <87503372+96kaneo@users.noreply.github.com>
2023-02-16 15:04:01 +1100

    My People Counter

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById('save-el')
+
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let saveCount = count + ' - '
+    saveEl.textContent += saveCount
+    count = 0
+    countEl.textContent = 0
+}
+
+function reset() {
+
+}
